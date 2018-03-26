eff9fa8fcfd2e7d75bf41c187b7afd87b0479330
android-LPJ <18434361814@163.com>
2018-03-26 16:35:11 +0800

    修改kafka-monitor的监控
    修改监控的参数判断方式

--- kafka/kafka-monitor.sh
+++ kafka/kafka-monitor.sh
@@ -7,15 +7,17 @@ ulimit -c unlimited
 command="timeout 30 java -Xms100m -Xmx100m -jar ./jmxcmd.jar - 172.19.8.44:9999"
 for i in $(cat ./list)
 do
-    name=$(echo "$i" | awk -F ":" '{print $2}' | awk -F "," '{print $1}' | awk -F "=" '{print $2}')
-    result=$($command "$i" Value 2>&1)
-    case $? in
-    0)
-    result=$(echo "$result" | cut -d ":" -f4|sed 's/^[ \t]*//g')
+    name=$(echo $i | awk -F ":" '{print $2}' | awk -F "," '{print $1}' | awk -F "=" '{print $2}')
+    result=$($command $i 2>&1)
+    case ${result} in
+    *Value*)
+    result=$($command $i Value 2>&1 | cut -d ":" -f4|sed 's/^[ \t]*//g')
+    ;;
+    *OneMinuteRate*)
+    result=$($command $i OneMinuteRate 2>&1 | cut -d ":" -f4| sed 's/^[ \t]*//g')
     ;;
     *)
-    result=$($command "$i" OneMinuteRate 2>&1 | cut -d ":" -f4|sed 's/^[ \t]*//g')
-    ;;
+    result="参数错误!"
     esac
     echo ${name} ": "${result}
 done
