19a2e84e8024ddff54463ba30bf49c17d6094108
android-LPJ <18434361814@163.com>
2018-03-27 20:54:29 +0800

    修改格式化json
    增加了： “字符串”
    增加了递归式格式化多层字典

--- elasticsearch/elasticsearch-monitor.py
+++ elasticsearch/elasticsearch-monitor.py
@@ -1,24 +1,38 @@
-#!/usr/bin/python2
-# -*- coding: utf-8 -*-
-# @Time    : 2018/3/26 18:10
+#!/usr/bin/python
+# coding=utf-8
+# @Time    : 2018/3/27 18:10
 # @Author  : lipeijing
 # @Email   : lipeijing@jd.com
 
+from __future__ import unicode_literals
 import os
 import json
 from pprint import pprint
 import commands
 import socket
 
+def isNumber(item):
+    try:
+        float(item)
+        return True
+    except Exception as e:
+        return False
+
+def list_dic(dic):
+    for name, value in dic.items():
+        if isinstance(value, dict):
+            list_dic(value)
+        else:
+            if isNumber(value):
+                print(name + ":" + str(value))
+            else:
+                print(name + ": \"" + value + "\"")
+
 if __name__ == "__main__":
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
-    try:
-        url = 'http://{ip}:9200/_cluster/health'.format(ip=ip)
-        cmd = 'curl --connect-timeout 5 -s "{url}"'.format(url=url)
-        (status, output) = commands.getstatusoutput(cmd)
-        result = json.loads(output)
-        for item in result:
-            print item + ":" + str(result[item])
-    except Exception as e:
-        print e
+    url = 'http://{ip}:9200/_cluster/health'.format(ip=ip)
+    cmd = 'curl -s "{url}"'.format(url=url)
+    (status, output) = commands.getstatusoutput(cmd)
+    result = json.loads(output)
+    list_dic(result)
