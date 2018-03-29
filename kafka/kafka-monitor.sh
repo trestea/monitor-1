af9384d102905a4292dcbdb69107f1e941cdaf2b
lipeijing <18434361814@163.com>
2018-03-29 10:24:26 +0800

    更新监控脚本
    将相对路径写成了绝对路径

--- kafka/kafka-monitor.sh
+++ kafka/kafka-monitor.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-set encoding=utf-8
+set encoding="utf-8"
 ulimit -m unlimited
 ulimit -v unlimited
 ulimit -c unlimited
 
-command="timeout 30 java -Xms100m -Xmx100m -jar ./jmxcmd.jar - localhost:9999"
-for i in $(cat ./list)
+JAVA="/export/App/jdk1.8.0_60/bin/java"
+HOST_IP=$(ifconfig eth0 | grep -P "inet (\d+)\.(\d+)\.(\d+)\.(\d+)" -o | grep -P "(\d+)\.(\d+)\.(\d+)\.(\d+)" -o)
+LIST="/opbin/list"
+JMX_JAR="/opbin/jmxcmd.jar"
+
+command="timeout 30 $JAVA -Xms100m -Xmx100m -jar $JMX_JAR - $HOST_IP:9999"
+for i in $(cat $LIST)
 do
     name=$(echo $i | awk -F ":" '{print $2}' | awk -F "," '{print $1}' | awk -F "=" '{print $2}')
     result=$($command $i 2>&1)
