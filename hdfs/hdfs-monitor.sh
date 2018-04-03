01e50b41d5a0b087028f29bf80a62b9a9a73f4f9
jiaozhenqing <jiaozhenqing@gmail.com>
2018-04-03 20:17:04 +0800

    update

--- hdfs/hdfs-monitor.sh
+++ hdfs/hdfs-monitor.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+# echo xdata > xdata
+# ./hadoop fs -mkdir /monitor 
+# ./hadoop fs -copyFromLocal ./xdata /monitor/
+
+result=$(timeout 30 /export/App/hadoop-2.6.1/bin/hadoop fs -cat /monitor/xdata 2>/dev/null)
+if [ "$result" = "xdata" ];then
+    echo "hdfs_read:0"
+else
+    echo "hdfs_read:1"
+fi
 
